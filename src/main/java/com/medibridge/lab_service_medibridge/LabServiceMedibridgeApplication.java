f9d87f0cf43acd984e49dcfe1a17cdf8b043444f
omprakashsaggam <omprakash.saggam@gmail.com>
2026-01-20 14:00:31 +0100

    initial commit

---
+++ src/main/java/com/medibridge/lab_service_medibridge/LabServiceMedibridgeApplication.java
@@ -0,0 +1,13 @@
+package com.medibridge.lab_service_medibridge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabServiceMedibridgeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LabServiceMedibridgeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/medibridge/lab_service_medibridge/LabServiceMedibridgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medibridge.lab_service_medibridge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabServiceMedibridgeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
